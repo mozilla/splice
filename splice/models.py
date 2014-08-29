1f61d063665d7fe0e36a5d02d6b70284f718c723
Olivier Yiptong <olivier@olivieryiptong.com>
2014-08-29 14:44:16 -0400

    remove all indexes

--- splice/models.py
+++ splice/models.py
@@ -19,9 +19,9 @@ class Tile(db.Model):
     image_uri = db.Column(db.Text(), nullable=False)
     enhanced_image_uri = db.Column(db.Text(), nullable=True)
 
-    locale = db.Column(db.String(14), nullable=False, index=True)
+    locale = db.Column(db.String(14), nullable=False)
 
-    created_at = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow, index=True)
+    created_at = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
 
 
 class UniqueCountsDaily(db.Model):
@@ -30,22 +30,22 @@ class UniqueCountsDaily(db.Model):
     id = db.Column(db.Integer(), autoincrement=True, primary_key=True, info={"identity": [0, 1]})
     tile_id = db.Column(db.Integer(), db.ForeignKey("tiles.id"))
 
-    date = db.Column(db.Date(), nullable=False, default=current_date, index=True)
+    date = db.Column(db.Date(), nullable=False, default=current_date)
     impression = db.Column(db.Boolean(), nullable=False, default=True)
-    locale = db.Column(db.String(14), nullable=False, default="en-US", index=True)
-    country_code = db.Column(db.String(5), nullable=False, default="US", index=True)
+    locale = db.Column(db.String(14), nullable=False, default="en-US")
+    country_code = db.Column(db.String(5), nullable=False, default="US")
 
 unique_hlls = db.Table(
     'unique_hlls',
     db.Column('unique_counts_daily_id', db.Integer, db.ForeignKey('unique_counts_daily.id')),
-    db.Column('index', db.SmallInteger, nullable=False, server_default="0", index=True),
-    db.Column('value', db.SmallInteger, nullable=False, server_default="0", index=True),
+    db.Column('index', db.SmallInteger, nullable=False, server_default="0"),
+    db.Column('value', db.SmallInteger, nullable=False, server_default="0"),
 )
 
 impression_stats_daily = db.Table(
     'impression_stats_daily',
     db.Column('tile_id', db.Integer, db.ForeignKey('tiles.id')),
-    db.Column('date', db.Date, index=True, nullable=False),
+    db.Column('date', db.Date, nullable=False),
     db.Column('impressions', db.Integer, nullable=False, server_default="0"),
     db.Column('clicks', db.Integer, nullable=False, server_default="0"),
     db.Column('pinned', db.Integer, nullable=False, server_default="0"),
@@ -53,10 +53,10 @@ impression_stats_daily = db.Table(
     db.Column('sponsored_link', db.Integer, nullable=False, server_default="0"),
     db.Column('sponsored', db.Integer, nullable=False, server_default="0"),
     db.Column('position', db.Integer, nullable=False, server_default="0"),
-    db.Column('locale', db.String(14), index=True, nullable=False),
-    db.Column('country_code', db.String(5), index=True, nullable=False),
-    db.Column('os', db.String(64), index=True, nullable=False),
-    db.Column('browser', db.String(64), index=True, nullable=False),
-    db.Column('version', db.String(64), index=True, nullable=False),
-    db.Column('device', db.String(64), index=True, nullable=False),
+    db.Column('locale', db.String(14), nullable=False),
+    db.Column('country_code', db.String(5), nullable=False),
+    db.Column('os', db.String(64), nullable=False),
+    db.Column('browser', db.String(64), nullable=False),
+    db.Column('version', db.String(64), nullable=False),
+    db.Column('device', db.String(64), nullable=False),
 )
