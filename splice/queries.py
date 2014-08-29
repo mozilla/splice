ab8ea65178da8e9ce17572edbbe0070d5fca2c2e
tspurway <tspurway@gmail.com>
2014-08-29 12:43:59 -0400

    added all slot and tile queries

--- splice/queries.py
+++ splice/queries.py
@@ -3,16 +3,18 @@ from sqlalchemy.sql import text
 from splice.environment import Environment
 from splice.models import Tile, impression_stats_daily
 from sqlalchemy.sql import select, func, and_
-env = Environment.instance()
 
 
+def tile_exists(target_url, bg_color, title, type, image_uri, enhanced_image_uri, locale, country_code, *args, **kwargs):
 
 def tile_exists(target_url, bg_color, title, type, image_uri, enhanced_image_uri, locale, *args, **kwargs):
     """
     Return the id of a tile having the data provided
     """
+    from splice.environment import Environment
+    env = Environment.instance()
     results = (
-        env.db.session
+        env.session
         .query(Tile.id)
         .filter(Tile.target_url == target_url)
         .filter(Tile.bg_color == bg_color)
@@ -29,13 +31,47 @@ def tile_exists(target_url, bg_color, title, type, image_uri, enhanced_image_uri
     return results
 
 
-def tile_stats_weekly(connection, start_date, tile_id=None):
+def _stats_query(connection, start_date, date_window, group_column_name, group_value):
     imps = impression_stats_daily
-    week_func_table = func.date_part('week', imps.c.date)
-    week_func_param = func.date_part('week', func.date(start_date))
-    if tile_id is not None:
-        where_clause = and_()
+    window_func_table = func.date_part(date_window, imps.c.date)
+    window_func_param = func.date_part(date_window, func.date(start_date))
+    group_column = imps.c.get(group_column_name)
+    if group_value is not None:
+        where_clause = and_(
+            window_func_table >= window_func_param,
+            group_column == group_value)
+    else:
+        where_clause = window_func_table >= window_func_param
+
     stmt = select([
+            window_func_table,
+            group_column,
+            func.sum(imps.c.impressions),
+            func.sum(imps.c.clicks),
+            func.sum(imps.c.pinned),
+            func.sum(imps.c.blocked),
+            func.sum(imps.c.sponsored),
+            func.sum(imps.c.sponsored_link)])\
+        .where(where_clause)\
+        .group_by(window_func_table, group_column)
+    return connection.execute(stmt)
+
+
+def tile_stats_weekly(connection, start_date, tile_id=None):
+    return _stats_query(connection, start_date, 'week', 'tile_id', tile_id)
+
+
+def tile_stats_monthly(connection, start_date, tile_id=None):
+    return _stats_query(connection, start_date, 'month', 'tile_id', tile_id)
+
+
+def slot_stats_weekly(connection, start_date, slot_id=None):
+    return _stats_query(connection, start_date, 'week', 'slot_id', slot_id)
+
+
+def slot_stats_monthly(connection, start_date, slot_id=None):
+    return _stats_query(connection, start_date, 'month', 'slot_id', slot_id)
+
         week_func_table, imps.c.tile_id,
         func.sum(imps.c.impressions),
         func.sum(imps.c.clicks),
