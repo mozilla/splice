c171cbddcabe0291a6ce432480f0be2395e08ea9
Olivier Yiptong <olivier@olivieryiptong.com>
2014-09-18 16:13:10 -0400

    remove dependencies from setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,6 @@
 from setuptools import setup, find_packages
 import os
 
-requires = [
-    "Flask==0.10.1",
-    "Flask-SQLAlchemy==1.0",
-    "Flask-Script==2.0.5",
-    "Flask-Migrate==1.2.0",
-    "gevent==1.0",
-    "gunicorn==18.0",
-    "boto==2.32.1",
-    "psycopg2==2.5.3",
-    "ujson==1.33",
-    "jsonschema==2.4.0",
-]
-
-if 'MOZ_SPLICE_DEV' in os.environ:
-    requires.extend([
-        "ipython==2.0.0",
-        "nose==1.3.1",
-        "flake8==2.1.0",
-        "Flask-Testing==0.4.1",
-        "Fabric==1.8.1",
-        "colorlog==2.0.0",
-    ])
-
 setup(
     name="splice",
     version="1.0.0",
