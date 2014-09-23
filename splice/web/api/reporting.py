8a040ab7051835d02038d565faa32a436618c3ea
tspurway <tspurway@gmail.com>
2014-09-23 16:43:48 -0400

    refactored module initialization

--- splice/web/api/reporting.py
+++ splice/web/api/reporting.py
@@ -1,7 +1,40 @@
-from flask import Blueprint, request
-from splice.web.api import build_response
-from splice.queries import *
-from splice.models import *
+from flask import Blueprint, request, Response
+from splice.queries import tile_stats_monthly, tile_stats_weekly, slot_stats_weekly, slot_stats_monthly
+from splice.models import Environment
+
+import ujson
+import StringIO
+import csv
+
+
+def _build_response(it, keys, name=''):
+    json = request.args.get('json', False)
+    if json == 'True':
+        json = True
+    headers = request.args.get('headers', True)
+    if headers == 'True':
+        headers = True
+    download = request.args.get('download', False)
+    if download == 'True':
+        download = True
+
+    if json:
+        rval = []
+        for tup in it:
+            rval.append(dict(zip(keys, tup)))
+        response = Response(ujson.dumps(rval), content_type='application/json; charset=utf-8', status=200)
+    else:
+        buf = StringIO.StringIO()
+        writer = csv.writer(buf)
+        if headers:
+            writer.writerow(keys)
+        for tup in it:
+            writer.writerow(tup)
+        response = Response(buf.getvalue(), content_type='text/csv; charset=utf-8', status=200)
+
+    if download:
+        response.headers['Content-Disposition'] = 'attachment; filename=imps_%s.csv' % name
+    return response
 
 report = Blueprint('api.report', __name__, url_prefix='/api/report')
 
@@ -16,7 +49,7 @@ def root():
 def path_tile_stats_weekly(start_date, tile_id=None):
     conn = Environment.instance().db.engine.connect()
     keys, rval = tile_stats_weekly(conn, start_date, tile_id)
-    return build_response(request, rval, keys, name=start_date)
+    return _build_response(rval, keys, name=start_date)
 
 
 @report.route('/tile_stats/monthly/<start_date>/<tile_id>', methods=['GET'])
@@ -24,7 +57,7 @@ def path_tile_stats_weekly(start_date, tile_id=None):
 def path_tile_stats_monthly(start_date, tile_id=None):
     conn = Environment.instance().db.engine.connect()
     keys, rval = tile_stats_monthly(conn, start_date, tile_id)
-    return build_response(request, rval, keys, name=start_date)
+    return _build_response(rval, keys, name=start_date)
 
 
 @report.route('/slot_stats/weekly/<start_date>/<slot_id>', methods=['GET'])
@@ -32,7 +65,7 @@ def path_tile_stats_monthly(start_date, tile_id=None):
 def path_slot_stats_weekly(start_date, slot_id=None):
     conn = Environment.instance().db.engine.connect()
     keys, rval = slot_stats_weekly(conn, start_date, slot_id)
-    return build_response(request, rval, keys, name=start_date)
+    return _build_response(rval, keys, name=start_date)
 
 
 @report.route('/slot_stats/monthly/<start_date>/<slot_id>', methods=['GET'])
@@ -40,7 +73,7 @@ def path_slot_stats_weekly(start_date, slot_id=None):
 def path_slot_stats_monthly(start_date, slot_id=None):
     conn = Environment.instance().db.engine.connect()
     keys, rval = slot_stats_monthly(conn, start_date, slot_id)
-    return build_response(request, rval, keys, name=start_date)
+    return _build_response(rval, keys, name=start_date)
 
 
 def register_routes(app):
