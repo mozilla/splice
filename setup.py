a0e16a3c1ae4bf6aa49b6673790a84bf2acafd7c
Olivier Yiptong <olivier@olivieryiptong.com>
2014-09-26 15:27:03 -0400

    version bump to 1.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name="splice",
-    version="1.0.1",
+    version="1.0.2",
     description="Link authoring tool for " +
                 "Firefox Directory and Enhanced Tiles",
     author="Mozilla",
