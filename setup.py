6da650bd2cb99ef138dfd7edd3964346064798fc
tspurway <tspurway@gmail.com>
2014-10-08 14:52:29 -0400

    Squashed commit of the following:
    commit 53e71f70980a89d55c353171d06ea138afcfb77f
    Author: Olivier Yiptong <olivier@olivieryiptong.com>
    Date:   Tue Oct 7 15:31:22 2014 -0400
    
        version bump to 1.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name="splice",
-    version="1.0.5",
+    version="1.0.6",
     description="Link authoring tool for " +
                 "Firefox Directory and Enhanced Tiles",
     author="Mozilla",
