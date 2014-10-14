6318e227ad5e84dedf4efb94deae77e9829cf56b
tspurway <tspurway@gmail.com>
2014-10-14 17:08:01 -0400

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name="splice",
-    version="1.0.6",
+    version="1.0.7",
     description="Link authoring tool for " +
                 "Firefox Directory and Enhanced Tiles",
     author="Mozilla",
