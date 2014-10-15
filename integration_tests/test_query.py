9bd4147f1a708b529e83ac67e00e172db56fa816
tspurway <tspurway@gmail.com>
2014-10-15 09:00:26 -0400

    adding 'daily' queries to integration test

--- integration_tests/test_query.py
+++ integration_tests/test_query.py
@@ -4,13 +4,19 @@ from splice.environment import Environment
 env = Environment.instance()
 
 from splice.queries import tile_stats_weekly, slot_stats_weekly, tile_stats_monthly, slot_stats_monthly, \
-    tile_summary_weekly, slot_summary_weekly, tile_summary_monthly, slot_summary_monthly
+    tile_summary_weekly, slot_summary_weekly, tile_summary_monthly, slot_summary_monthly, \
+    tile_stats_daily, tile_summary_daily
 
 with env.application.app_context():
     # TODO: check results
     conn = env.db.engine.connect()
     print "\ntile_summary_weekly"
     key, rval = tile_summary_weekly(conn, '2014-05-15')
+    for year, week, tile_id, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
+        print year, week, tile_id, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+
+    print "\ntile_summary_daily"
+    _, rval = tile_summary_daily(conn, '2014-05-15')
     for year, week, tile_id, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
         print year, week, tile_id, imps, clicks, pinned, blocked, spon, spon_link, newtabs
 
@@ -21,6 +27,16 @@ with env.application.app_context():
 
     print "\ntile_stats_weekly - tile_id = 2, country_code = US"
     _, rval = tile_stats_weekly(conn, '2014-05-15', '2', 'US')
+    for year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
+        print year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+
+    print "\ntile_stats_daily - tile_id = 2"
+    _, rval = tile_stats_daily(conn, '2014-05-15', '2')
+    for year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
+        print year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+
+    print "\ntile_stats_daily - tile_id = 2, country_code = US"
+    _, rval = tile_stats_daily(conn, '2014-05-15', '2', 'US')
     for year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
         print year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs
 
