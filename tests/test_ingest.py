a95108c6458ad340ded6ad23818f5e28431776e4
Olivier Yiptong <olivier@olivieryiptong.com>
2014-10-17 15:54:39 -0400

    additional test for generate_artifacts in test_ingest

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -1,6 +1,6 @@
 from nose.tools import assert_raises, assert_equal, assert_not_equal
 from jsonschema.exceptions import ValidationError
-from splice.ingest import ingest_links, IngestError
+from splice.ingest import ingest_links, generate_artifacts, IngestError
 from tests.base import BaseTestCase
 
 
@@ -101,3 +101,64 @@ class TestIngestLinks(BaseTestCase):
         assert_equal(1, directory_id_star)
         assert_not_equal(data["STAR/en-US"][1]["directoryId"], directory_id_star)
         assert_equal(directory_id_ca, directory_id_star)
+
+    def test_id_not_overwritten(self):
+        """
+        Test an id is created for a valid tile
+        """
+        tiles_star = [
+            {
+                "imageURI": "https://somewhere.com/image.png",
+                "url": "https://somewhere.com",
+                "title": "Some Title",
+                "type": "organic",
+                "bgColor": "#FFFFFF"
+            }
+        ]
+
+        data = ingest_links({"STAR/en-US": tiles_star})
+        directory_id = data["STAR/en-US"][0]["directoryId"]
+        assert_equal(1, directory_id)
+
+        data = ingest_links({"STAR/en-US": tiles_star})
+        directory_id = data["STAR/en-US"][0]["directoryId"]
+        assert_equal(1, directory_id)
+
+
+class TestGenerateArtifacts(BaseTestCase):
+
+    def test_generate_artifacts(self):
+        """
+        Tests that the correct number of artifacts are generated
+        """
+        tiles_star = [
+            {
+                "imageURI": "https://somewhere.com/image.png",
+                "url": "https://somewhere.com",
+                "title": "Some Title",
+                "type": "organic",
+                "bgColor": "#FFFFFF"
+            }
+        ]
+
+        tiles_ca = [
+            {
+                "imageURI": "https://somewhere.com/image.png",
+                "url": "https://somewhere.com",
+                "title": "Some Title",
+                "type": "organic",
+                "bgColor": "#FFFFFF"
+            }
+        ]
+
+        data = ingest_links({"STAR/en-US": tiles_star})
+        artifacts = generate_artifacts(data)
+        # tile index and distribution files are generated
+        assert_equal(3, len(artifacts))
+
+        data = ingest_links({
+            "STAR/en-US": tiles_star,
+            "CA/en-US": tiles_ca,
+        })
+        artifacts = generate_artifacts(data)
+        assert_equal(4, len(artifacts))
