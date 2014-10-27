4de0e61863227e4bb3d00bd7135ba65aa189edbd
tspurway <tspurway@gmail.com>
2014-10-27 11:29:40 -0400

    closes #24 fixes #23
    Squashed commit of the following:
    
    commit 9874654b6966aaec4ba9095fcb5dafd1df293560
    Author: tspurway <tspurway@gmail.com>
    Date:   Fri Oct 24 16:32:07 2014 -0400
    
        integrated this feature branch, finished it's testcases and fixtures
    
    commit 662e302597d5f293fe42e6c994af1505d6e2ef97
    Merge: a2b51a8 4ad67cb
    Author: tspurway <tspurway@gmail.com>
    Date:   Fri Oct 24 14:07:25 2014 -0400
    
        Merge branch 'master' into issue-23-newtabs-table
    
        Conflicts:
        	integration_tests/test_query.py
        	splice/models.py
        	splice/queries.py
        	tests/api/test_reporting.py
    
    commit a2b51a8898464e1db4f9ab8539d79ec4138274c9
    Author: tspurway <tspurway@gmail.com>
    Date:   Thu Oct 23 00:18:02 2014 -0400
    
        refactored / simplified queries to accomodate newtab_stats_daily table
        (cherry picked from commit 63e126d)
    
    commit ec8c84d2d76db972fb3782a11e71b44d24cea969
    Author: tspurway <tspurway@gmail.com>
    Date:   Wed Oct 22 17:58:13 2014 -0400
    
        adding data migration for new table, newtab_stats_daily.  Migrating newtab data from impression_stats_daily to new table.  Deleting old data from impression_stats_daily, and deleting old column newtabs.
        (cherry picked from commit 6f9095d)

--- integration_tests/test_query.py
+++ integration_tests/test_query.py
@@ -17,70 +17,70 @@ with env.application.app_context():
 
     print "\ntile_summary_daily"
     _, rval = tile_summary_daily(conn, '2014-05-15')
-    for year, week, tile_id, title, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
-        print year, week, tile_id, title, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+    for year, week, tile_id, title, imps, clicks, pinned, blocked, spon, spon_link in rval:
+        print year, week, tile_id, title, imps, clicks, pinned, blocked, spon, spon_link
 
     print "\ntile_stats_weekly - tile_id = 2"
     _, rval = tile_stats_weekly(conn, '2014-05-15', '2')
-    for year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
-        print year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+    for year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link in rval:
+        print year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link
 
     print "\ntile_stats_weekly - tile_id = 2, country_code = US"
     _, rval = tile_stats_weekly(conn, '2014-05-15', '2', 'US')
-    for year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
-        print year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+    for year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link in rval:
+        print year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link
 
     print "\ntile_stats_daily - tile_id = 2"
     _, rval = tile_stats_daily(conn, '2014-05-15', '2')
-    for year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
-        print year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+    for year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link in rval:
+        print year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link
 
     print "\ntile_stats_daily - tile_id = 2, country_code = US"
     _, rval = tile_stats_daily(conn, '2014-05-15', '2', 'US')
-    for year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
-        print year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+    for year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link in rval:
+        print year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link
 
     print "\nslot_summary_weekly"
     _, rval = slot_summary_weekly(conn, '2014-05-15')
-    for year, week, tile_id, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
-        print year, week, tile_id, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+    for year, week, tile_id, imps, clicks, pinned, blocked, spon, spon_link in rval:
+        print year, week, tile_id, imps, clicks, pinned, blocked, spon, spon_link
 
     print "\nslot_stats_weekly - slot_id = 1"
     _, rval = slot_stats_weekly(conn, '2014-05-15', 1)
-    for year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
-        print year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+    for year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link in rval:
+        print year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link
 
     print "\nslot_stats_weekly - slot_id = 1, country_code = US"
     _, rval = slot_stats_weekly(conn, '2014-05-15', 1, 'US')
-    for year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
-        print year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+    for year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link in rval:
+        print year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link
 
     print "\ntile_summary_monthly"
     _, rval = tile_summary_monthly(conn, '2014-05-15')
-    for year, week, tile_id, title, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
-        print year, week, tile_id, title, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+    for year, week, tile_id, title, imps, clicks, pinned, blocked, spon, spon_link in rval:
+        print year, week, tile_id, title, imps, clicks, pinned, blocked, spon, spon_link
 
     print "\ntile_stats_monthly - tile_id = 2"
     _, rval = tile_stats_monthly(conn, '2014-05-15', '2')
-    for year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
-        print year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+    for year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link in rval:
+        print year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link
 
     print "\ntile_stats_monthly - tile_id = 2, country_code = US"
     _, rval = tile_stats_monthly(conn, '2014-05-15', '2', 'US')
-    for year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
-        print year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+    for year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link in rval:
+        print year, week, tile_id, title, country, locale, imps, clicks, pinned, blocked, spon, spon_link
 
     print "\nslot_summary_monthly"
     _, rval = slot_summary_monthly(conn, '2014-05-15')
-    for year, week, tile_id, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
-        print year, week, tile_id, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+    for year, week, tile_id, imps, clicks, pinned, blocked, spon, spon_link in rval:
+        print year, week, tile_id, imps, clicks, pinned, blocked, spon, spon_link
 
     print "\nslot_stats_monthly - slot_id = 1"
     _, rval = slot_stats_monthly(conn, '2014-05-15', 1)
-    for year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
-        print year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+    for year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link in rval:
+        print year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link
 
     print "\nslot_stats_monthly - slot_id = 1, country_code = US"
     _, rval = slot_stats_monthly(conn, '2014-05-15', 1, 'US')
-    for year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs in rval:
-        print year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link, newtabs
+    for year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link in rval:
+        print year, week, tile_id, country, locale, imps, clicks, pinned, blocked, spon, spon_link
--- tests/api/test_reporting.py
+++ tests/api/test_reporting.py
@@ -12,28 +12,36 @@ class TestReporting(BaseTestCase):
     def setUp(self):
         super(TestReporting, self).setUp()
 
-        def values(fd):
+        def values(fd, date_index=0):
             for line in fd:
                 row = line.split(',')
                 # sqlalchemy doesn't like date strings....
-                row[1] = datetime.strptime(row[1], "%Y-%m-%d")
+                row[date_index] = datetime.strptime(row[date_index], "%Y-%m-%d")
                 yield row
 
         # load db
-        from splice.models import impression_stats_daily
+        from splice.models import impression_stats_daily, newtab_stats_daily
         conn = Environment.instance().db.engine.connect()
+
         with open(self.get_fixture_path('impression_stats.csv')) as fd:
-            for row in values(fd):
+            for row in values(fd, 1):
                 ins = impression_stats_daily.insert().values(row)
+                conn.execute(ins)
+
+        with open(self.get_fixture_path('newtabs.csv')) as fd:
+            for row in values(fd):
+                ins = newtab_stats_daily.insert().values(row)
                 conn.execute(ins)
 
     def test_tile_summary_weekly(self):
         """
         /tile_summary/weekly/<start_date>
         """
-        url = url_for('api.report.path_tile_summary_weekly',
+        url = url_for('api.report.path_summary',
                       start_date='2014-05-15',
-                      headers='false')
+                      headers='false',
+                      summary='tile',
+                      period='weekly')
         response = self.client.get(url)
         assert_equal(response.status_code, 200)
 
@@ -47,13 +55,13 @@ class TestReporting(BaseTestCase):
             id = tuple(int(r) for r in row[:3])
             vals = tuple(int(r) for r in row[4:])
             if id == (2014, 36, 11):
-                assert_equal(vals, (1, 0, 0, 0, 0, 0, 0))
+                assert_equal(vals, (1, 0, 0, 0, 0, 0))
                 f1 = True
             elif id == (2014, 39, 12):
-                assert_equal(vals, (230, 2, 0, 1, 0, 0, 0))
+                assert_equal(vals, (230, 2, 0, 1, 0, 0))
                 f2 = True
             elif id == (2014, 40, 15):
-                assert_equal(vals, (2162, 2, 0, 12, 0, 0, 0))
+                assert_equal(vals, (2162, 2, 0, 12, 0, 0))
                 f3 = True
 
         assert(f1 and f2 and f3)
@@ -62,10 +70,12 @@ class TestReporting(BaseTestCase):
         """
         /tile_summary/daily/<start_date>/<country_code>
         """
-        url = url_for('api.report.path_tile_summary_daily',
+        url = url_for('api.report.path_summary',
                       start_date='2014-10-15',
                       headers='false',
-                      country_code='US')
+                      country_code='US',
+                      period='daily',
+                      summary='tile')
         response = self.client.get(url)
         assert_equal(response.status_code, 200)
 
@@ -81,10 +91,10 @@ class TestReporting(BaseTestCase):
             if id == ('2014', '2014-09-26', '15'):
                 f1 = True
             elif id == ('2014', '2014-10-16', '18'):
-                assert_equal(vals, ('259', '0', '0', '0', '0', '0', '0'))
+                assert_equal(vals, ('259', '0', '0', '0', '0', '0'))
                 f2 = True
             elif id == ('2014', '2014-10-22', '19'):
-                assert_equal(vals, ('4', '0', '0', '0', '0', '0', '0'))
+                assert_equal(vals, ('4', '0', '0', '0', '0', '0'))
                 f3 = True
 
         assert(f2 and f3)
@@ -94,10 +104,12 @@ class TestReporting(BaseTestCase):
         """
         /tile_summary/daily/<start_date>/<country_code>
         """
-        url = url_for('api.report.path_slot_summary_weekly',
+        url = url_for('api.report.path_summary',
                       start_date='2014-10-10',
                       headers='false',
-                      country_code='US')
+                      country_code='US',
+                      summary='slot',
+                      period='weekly')
         response = self.client.get(url)
         assert_equal(response.status_code, 200)
 
@@ -113,10 +125,10 @@ class TestReporting(BaseTestCase):
             if id == ('2014', '40', '12'):
                 f1 = True
             elif id == ('2014', '42', '7'):
-                assert_equal(vals, ('259', '0', '0', '0', '0', '0', '0'))
+                assert_equal(vals, ('259', '0', '0', '0', '0', '0'))
                 f2 = True
             elif id == ('2014', '43', '4'):
-                assert_equal(vals, ('4', '0', '0', '0', '0', '0', '0'))
+                assert_equal(vals, ('4', '0', '0', '0', '0', '0'))
                 f3 = True
 
         assert(f2 and f3)
@@ -126,10 +138,11 @@ class TestReporting(BaseTestCase):
         """
         /tile_stats/daily/<start_date>/<tile_id>
         """
-        url = url_for('api.report.path_tile_stats_daily',
+        url = url_for('api.report.path_tile_stats',
                       start_date='2014-10-20',
                       headers='false',
-                      tile_id=19)
+                      tile_id=19,
+                      period='daily')
         response = self.client.get(url)
         assert_equal(response.status_code, 200)
 
@@ -145,10 +158,10 @@ class TestReporting(BaseTestCase):
             if id == ('2014', '2014-09-24', '19', "Firefox Marketplace", 'GB', 'en-US'):
                 f1 = True
             elif id == ('2014', '2014-10-20', '19', "Firefox Marketplace", 'ID', 'en-US'):
-                assert_equal(vals, ('314', '0', '0', '0', '0', '0', '0'))
+                assert_equal(vals, ('314', '0', '0', '0', '0', '0'))
                 f2 = True
             elif id == ('2014', '2014-10-23', '19', "Firefox Marketplace", 'RS', 'en-US'):
-                assert_equal(vals, ('2', '0', '0', '0', '0', '0', '0'))
+                assert_equal(vals, ('2', '0', '0', '0', '0', '0'))
                 f3 = True
 
         assert(f2 and f3)
@@ -158,11 +171,12 @@ class TestReporting(BaseTestCase):
         """
         /tile_stats/monthly/<start_date>/<tile_id>/<country_code>
         """
-        url = url_for('api.report.path_tile_stats_monthly',
+        url = url_for('api.report.path_tile_stats',
                       start_date='2014-09-20',
                       headers='false',
                       country_code='PH',
-                      tile_id=15)
+                      tile_id=15,
+                      period='monthly')
         response = self.client.get(url)
         assert_equal(response.status_code, 200)
 
@@ -175,10 +189,69 @@ class TestReporting(BaseTestCase):
             id = tuple(row[:6])
             vals = tuple(row[6:])
             if id == ('2014', '9', '15', "Firefox for Android", 'PH', 'en-US'):
-                assert_equal(vals, ('1651', '1', '0', '14', '0', '0', '0'))
+                assert_equal(vals, ('1651', '1', '0', '14', '0', '0'))
                 f1 = True
             elif id == ('2014', '10', '15', "Firefox for Android", 'PH', 'en-US'):
                 f2 = True
 
         assert(f1)
         assert(f2)
+
+    def test_newtab_stats_daily(self):
+        """
+        /newtab_stats/daily/<start_date>/
+        """
+        url = url_for('api.report.path_newtab_stats',
+                      start_date='2014-10-20',
+                      headers='false',
+                      period='daily')
+        response = self.client.get(url)
+        assert_equal(response.status_code, 200)
+
+        outtext = response.response.next()
+        results = csv.reader(StringIO(outtext))
+
+        f1 = False
+        f2 = False
+        f3 = False
+        for row in results:
+            id = tuple(row[:4])
+            val = row[4]
+            if id == ('2014', '2014-10-19', 'LT', 'lt'):
+                f1 = True
+            elif id == ('2014', '2014-10-21', 'PH', 'en-US'):
+                assert_equal(val, '3')
+                f2 = True
+            elif id == ('2014', '2014-10-23', 'LB', 'en-US'):
+                assert_equal(val, '2')
+                f3 = True
+        assert(f2 and f3)
+        assert(not f1)
+
+    def test_newtab_stats_monthly(self):
+        """
+        /newtab_stats/monthly/<start_date>/<country_code>
+        """
+        url = url_for('api.report.path_newtab_stats',
+                      start_date='2014-09-20',
+                      headers='false',
+                      country_code='US',
+                      period='monthly')
+        response = self.client.get(url)
+        assert_equal(response.status_code, 200)
+
+        outtext = response.response.next()
+        results = csv.reader(StringIO(outtext))
+
+        f1 = False
+        f2 = False
+        for row in results:
+            id = tuple(row[:4])
+            val = row[4]
+            if id == ('2014', '10', 'US', 'en-US'):
+                assert_equal(val, '51')
+                f1 = True
+            elif id == ('2014', '10', 'US', 'ja'):
+                assert_equal(val, '116')
+                f2 = True
+        assert(f1 and f2)
