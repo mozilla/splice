f75e6d7e85a886bf1522020fb5a817f78e19e2b5
tspurway <tspurway@gmail.com>
2014-10-30 17:11:30 -0400

    version bump 1.0.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="splice",
-    version="1.0.10",
+    version="1.0.11",
     description="Link authoring tool for " +
                 "Firefox Directory and Enhanced Tiles",
     author="Mozilla",
