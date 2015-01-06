5b54d1c999bfa34813b12164ec70f98c1c8099c3
tspurway <tspurway@gmail.com>
2015-01-06 16:18:29 -0500

    adding migration for site_stats_daily

---
+++ ddl/migrations/redshift/1420475188.sql
@@ -0,0 +1,19 @@
+BEGIN;
+
+CREATE TABLE site_stats_daily
+(
+    date DATE NOT NULL,
+    month INT DEFAULT 0 NOT NULL,
+    week INT DEFAULT 0 NOT NULL,
+    year INT DEFAULT 0 NOT NULL,
+    locale VARCHAR(14) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    country_code VARCHAR(5) NOT NULL,
+    os VARCHAR(64) NOT NULL,
+    browser VARCHAR(64) NOT NULL,
+    version VARCHAR(64) NOT NULL,
+    device VARCHAR(64) NOT NULL,
+    count INT DEFAULT 0 NOT NULL
+);
+
+COMMIT;
