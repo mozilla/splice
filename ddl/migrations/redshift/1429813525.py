b7e12ec23c1056140c10e39b576a94e3405b881f
tspurway <tspurway@gmail.com>
2015-04-23 16:56:18 -0400

    added context to the migration script to make it 'go'

--- ddl/migrations/redshift/1429813525.py
+++ ddl/migrations/redshift/1429813525.py
@@ -2,30 +2,33 @@
 from sqlalchemy.sql import text
 from splice.queries import get_tiles
 
+
 from splice.environment import Environment
 env = Environment.instance()
+with env.application.app_context():
 
-session = env.db.session
-conn = env.db.engine.connect()
-trans = conn.begin()
+    session = env.db.session
+    conn = env.db.engine.connect()
+    trans = conn.begin()
 
-try:
-    has_rows = conn.execute("SELECT count(*) FROM adgroups").scalar()
-    if has_rows:
-        print "won't execute insert.  adgroups has %d non-empty rows" % has_rows
+
+    try:
+        has_rows = conn.execute("SELECT count(*) FROM adgroups").scalar()
+        if has_rows:
+            print "won't execute insert.  adgroups has %d non-empty rows" % has_rows
+        else:
+            for tile in get_tiles():
+                conn.execute(
+                    text("INSERT INTO adgroups (locale) VALUES (:locale)"),
+                    locale=tile['locale'])
+                ag_id = conn.execute("SELECT MAX(id) FROM adgroups;").scalar()
+                conn.execute(
+                    text("UPDATE tiles set adgroup_id = :adgroup_id"),
+                    adgroup_id=ag_id)
+    except Exception as e:
+        trans.rollback()
+        print "error: %s" % e
     else:
-        for tile in get_tiles():
-            conn.execute(
-                text("INSERT INTO adgroups (locale) VALUES (:locale)"),
-                locale=tile['locale'])
-            ag_id = conn.execute("SELECT MAX(id) FROM adgroups;").scalar()
-            conn.execute(
-                text("UPDATE tiles set adgroup_id = :adgroup_id"),
-                adgroup_id=ag_id)
-except Exception as e:
-    trans.rollback()
-    print "error: %s" % e
-else:
-    trans.commit()
-    if not has_rows:
-        print "success!"
+        trans.commit()
+        if not has_rows:
+            print "success!"
