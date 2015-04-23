54fd27a9320177f6b3ebe8306b9f192dcde87de3
Daniel Thornton <dthornton@mozilla.com>
2015-04-23 14:57:03 -0700

    fix the migration

--- ddl/migrations/redshift/1429813525.sql
+++ ddl/migrations/redshift/1429813525.sql
@@ -3,7 +3,7 @@ BEGIN;
 CREATE TABLE adgroups
 (
     id INTEGER IDENTITY(1,1) NOT NULL,
-    created_at TIMESTAMP DEFAULT now() NOT NULL,
+    created_at TIMESTAMP DEFAULT SYSDATE NOT NULL,
     locale VARCHAR(14) NOT NULL,
     PRIMARY KEY (id)
 
@@ -14,7 +14,7 @@ CREATE TABLE adgroup_sites
     id INTEGER IDENTITY(1,1) NOT NULL,
     site VARCHAR(1024) NOT NULL,
     adgroup_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT now() NOT NULL,
+    created_at TIMESTAMP DEFAULT SYSDATE NOT NULL,
     FOREIGN KEY (adgroup_id) REFERENCES adgroups (id),
     PRIMARY KEY (id)
 
@@ -22,5 +22,9 @@ CREATE TABLE adgroup_sites
 
 ALTER TABLE tiles ADD COLUMN adgroup_id INT;
 ALTER TABLE tiles ADD FOREIGN KEY (adgroup_id) REFERENCES adgroups (id);
+
+-- initialize the adgroups table
+INSERT INTO adgroups (locale) SELECT locale FROM tiles ORDER BY id;
+UPDATE tiles SET adgroup_id = id;
 
 COMMIT;
