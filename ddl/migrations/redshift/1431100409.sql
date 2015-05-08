a71d4a78d67424bcce3ccc3ad1c7ab9b37740ae1
tspurway <tspurway@gmail.com>
2015-05-08 12:40:03 -0400

    database migrations for new tables: blacklisted_ips, blacklist_stats_daily

---
+++ ddl/migrations/redshift/1431100409.sql
@@ -0,0 +1,23 @@
+BEGIN;
+
+CREATE TABLE blacklisted_ips
+(
+    ip VARCHAR(16) NOT NULL,
+    created_at TIMESTAMP DEFAULT SYSDATE NOT NULL,
+    PRIMARY KEY (ip)
+);
+
+CREATE TABLE blacklist_stats_daily
+(
+    ip VARCHAR(16) NOT NULL,
+    date DATE NOT NULL,
+    impressions INTEGER DEFAULT '0' NOT NULL,
+    clicks INTEGER DEFAULT '0' NOT NULL,
+    pinned INTEGER DEFAULT '0' NOT NULL,
+    blocked INTEGER DEFAULT '0' NOT NULL,
+    sponsored_link INTEGER DEFAULT '0' NOT NULL,
+    sponsored INTEGER DEFAULT '0' NOT NULL,
+    newtabs INTEGER DEFAULT '0' NOT NULL
+);
+
+COMMIT;
