4712152896f3c84b9f54afdef462d56958fff6b2
Olivier Yiptong <olivier@olivieryiptong.com>
2015-06-15 12:06:17 -0400

    redshift migration: adgroup channel_id change for android tiles

--- ddl/migrations/redshift/1433974555.sql
+++ ddl/migrations/redshift/1433974555.sql
@@ -1,2 +1,4 @@
 ALTER TABLE adgroups ADD COLUMN channel_id INTEGER;
+UPDATE adgroups SET channel_id = 1;
+UPDATE adgroups SET channel_id = 2 WHERE id in (SELECT adgroup_id FROM tiles WHERE id in (529, 630, 631, 632));
 ALTER TABLE adgroups ADD FOREIGN KEY(channel_id) REFERENCES channels (id);
