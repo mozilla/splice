2c609a347a48cf8d1ec347787e7c31d30f0774e0
Olivier Yiptong <olivier@olivieryiptong.com>
2015-06-25 15:24:59 -0700

    Fix sql migration
    Squashed commit of the following:
    
    commit a7e77a9588e085b11cf09e49130507646ae26e3f
    Author: Daniel Thornton <dthornton@mozilla.com>
    Date:   Thu Jun 25 14:57:52 2015 -0700
    
        fix sql migration
    
    Closes #8

--- ddl/migrations/redshift/1433974555.sql
+++ ddl/migrations/redshift/1433974555.sql
@@ -1,6 +1,13 @@
+BEGIN;
+
 ALTER TABLE adgroups ADD COLUMN channel_id INTEGER;
-UPDATE adgroups SET channel_id = 1;
-UPDATE adgroups SET channel_id = 2 WHERE id in (SELECT adgroup_id FROM tiles WHERE id in (529, 630, 631, 632));
+UPDATE adgroups SET channel_id = (SELECT MIN(id) FROM channels WHERE name = 'desktop');
+UPDATE adgroups SET channel_id = (SELECT MIN(id) FROM channels WHERE name = 'android') WHERE id in (SELECT adgroup_id FROM tiles WHERE id in (529, 630, 631, 632));
 ALTER TABLE adgroups ADD FOREIGN KEY(channel_id) REFERENCES channels (id);
-ALTER TABLE channels ALTER COLUMN name TYPE VARCHAR(32);
-INSERT INTO channels (name) VALUES ('desktop-prerelease');
+ALTER TABLE channels RENAME COLUMN name TO name_short;
+ALTER TABLE channels ADD COLUMN name VARCHAR(32) DEFAULT 'error' NOT NULL;
+UPDATE channels SET name = name_short;
+INSERT INTO channels (name, name_short, created_at) VALUES ('desktop-prerelease', 'error', SYSDATE);
+ALTER TABLE channels DROP COLUMN name_short;
+
+COMMIT;
