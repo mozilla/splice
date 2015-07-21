6f6edfc30f1d99fb3f516fe4d9fc72386814c3d0
tspurway <tspurway@gmail.com>
2015-07-21 14:14:44 +0200

    adding a script to move data from old redshift based campaign tables to new

---
+++ scripts/rs_to_pg.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import psycopg2
+from splice.environment import Environment
+from StringIO import StringIO
+
+
+def from_ts(timestamp):
+    pass
+
+
+#
+# stuff
+#
+TABLES = (
+    ('adgroups', ('id', 'created_at', 'locale', 'frequency_cap_daily', 'frequency_cap_total', 'check_inadjacency', 'explanation', 'name', 'end_date', 'end_date_dt', 'start_date', 'start_date_dt')),
+    ('adgroup_sites', ('id', 'site', 'adgroup_id', 'created_at')),
+    ('tiles', ('id', 'target_url', 'bg_color', 'title', 'type', 'image_uri', 'enhanced_image_uri', 'created_at', 'adgroup_id', 'locale')),
+)
+
+
+def main():
+    print "So it begins..."
+    target_db_uri = Environment.instance().config.SQLALCHEMY_DATABASE_URI
+    target_connection = psycopg2.connect(target_db_uri)
+    target_cursor = target_connection.cursor()
+
+    db_uri = Environment.instance().config.SQLALCHEMY_BINDS['stats']
+    connection = psycopg2.connect(db_uri)
+    cursor = connection.cursor()
+
+    try:
+        for table_name, columns in TABLES:
+            print "Copying ", table_name
+
+            # first, we need to lock the source table
+            # cursor.execute("LOCK TABLE %s" % table_name)
+
+            # we need to assert the table is empty
+            target_cursor.execute("select count(*) from %s" % table_name)
+            count, = target_cursor.fetchone()
+            assert count == 0
+
+            col_string = ','.join(columns)
+            str_string = ','.join(["%s"] * len(columns))
+            target_query = StringIO()
+            target_query.write('insert into %s(%s) values ' % (table_name, col_string))
+            cursor.execute('select %s from %s' % (col_string, table_name))
+            for rec in cursor:
+                target_query.write("(%s)," % target_cursor.mogrify(str_string, tuple(rec)))
+            target_cursor.execute(target_query.getvalue()[:-1])
+
+            # now we need to reset the sequence associated with the id for this table
+            target_cursor.execute("select max(id) + 1 from %s" % table_name)
+            nextone, = target_cursor.fetchone()
+            target_cursor.execute("ALTER SEQUENCE %s_id_seq start %s" % (table_name, nextone))
+
+    except Exception as e:
+        print "Error ", e
+        target_connection.rollback()
+        connection.rollback()
+    else:
+        print "Good, well done, excellent."
+        target_connection.commit()
+        connection.commit()
+
+    finally:
+        connection.close()
+        target_connection.close()
+
+if __name__ == '__main__':
+    main()
+
