8500f5c5ba58cf2f7461d7eab32aedc4b69c65c2
tspurway <tspurway@gmail.com>
2015-09-17 16:18:53 -0400

    fixing double head in migrations

--- migrations/versions/16fa35dd63a2_.py
+++ migrations/versions/16fa35dd63a2_.py
@@ -8,7 +8,7 @@ Create Date: 2015-08-25 17:43:17.127488
 
 # revision identifiers, used by Alembic.
 revision = '16fa35dd63a2'
-down_revision = '19605520be19'
+down_revision = 'd8d5541170a'
 branch_labels = None
 depends_on = None
 
