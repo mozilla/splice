4084ea3c40b72844e5761210674b7a2e8c8507f4
Tawin Kiethanom <tawin.k@gmail.com>
2015-09-22 11:52:07 -0700

    Renamed to AccountViewPage.js

--- front_end/src/pages/index.js
+++ front_end/src/pages/index.js
@@ -1,7 +1,7 @@
 import AppPage from './AppPage.js';
 import HomePage from './HomePage.js';
 import AccountsPage from './Accounts/AccountsPage.js';
-import AccountsViewPage from './Accounts/AccountsViewPage.js';
+import AccountViewPage from './Accounts/AccountViewPage.js';
 import AccountsAddPage from './Accounts/AccountsAddPage.js';
 import CampaignsPage from './Campaigns/CampaignsPage.js';
 
@@ -9,7 +9,7 @@ export {
 	AppPage,
 	HomePage,
 	AccountsPage,
-	AccountsViewPage,
+	AccountViewPage,
 	AccountsAddPage,
 	CampaignsPage
 };
