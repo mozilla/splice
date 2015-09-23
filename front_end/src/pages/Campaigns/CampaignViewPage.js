a6bf74d5294d2a05e13434297883c17b0f586274
Tawin Kiethanom <tawin.k@gmail.com>
2015-09-22 18:17:03 -0700

    Generic Reducer variables. Added Ad Group Pages.

---
+++ front_end/src/pages/Campaigns/CampaignViewPage.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react/addons';
+
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+import { updateDocTitle, pageVisit } from 'actions/App/AppActions';
+
+import { fetchCampaign } from 'actions/Campaigns/CampaignActions';
+import { fetchAdGroups } from 'actions/AdGroups/AdGroupActions';
+
+import CampaignDetails from 'components/Campaigns/CampaignDetails/CampaignDetails';
+import AdGroupList from 'components/AdGroups/AdGroupList/AdGroupList';
+
+export default class CampaignViewPage extends Component {
+	componentWillMount() {
+		this.fetchCampaignDetails(this.props);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.params.campaignId !== this.props.params.campaignId) {
+			this.fetchCampaignDetails(nextProps);
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="row">
+					<div className="col-md-6">
+						<h1>Campaign</h1>
+						<CampaignDetails Campaign={this.props.Campaign}/>
+					</div>
+				</div>
+				<br/>
+				<strong>Ad Groups</strong>
+				<AdGroupList rows={this.props.AdGroup.rows}
+							  isFetching={this.props.AdGroup.isFetching}/>
+			</div>
+		);
+	}
+
+	fetchCampaignDetails(props){
+		const { dispatch } = props;
+		const data = props.Campaign.details;
+		const campaignId = parseInt(props.params.campaignId, 10);
+
+		updateDocTitle('Campaign View');
+
+		dispatch(fetchCampaign(campaignId)).then(() => {
+			pageVisit('Campaign - ' + this.props.Campaign.details.name, this);
+			dispatch(fetchAdGroups(this.props.Campaign.details.id));
+		});
+	}
+}
+
+CampaignViewPage.propTypes = {
+
+};
+
+function select(state) {
+	return {
+		Account: state.Account,
+		Campaign: state.Campaign,
+		AdGroup: state.AdGroup
+	};
+}
+
+// Wrap the component to inject dispatch and state into it
+export default connect(select)(CampaignViewPage);
