8f890f3896c283151b38018510c30154a9867295
Tawin Kiethanom <tawin.k@gmail.com>
2015-09-29 17:30:07 -0700

    Handling of any unknown route

--- front_end/src/routes.js
+++ front_end/src/routes.js
@@ -1,7 +1,7 @@
 /* eslint react/self-closing-comp:0 */
 
 import React, { Component } from 'react';
-import { Router, Route } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 import createHistory from 'history/lib/createHashHistory';
 // Use _key instead of _k.
 const history = createHistory({
@@ -29,26 +29,27 @@ export default class App extends Component {
   render() {
     return (
       <Router history={history}>
-        <Route component={AppPage}>
-          <Route path="/" title="Home" component={HomePage}></Route>
+        <Route path="/" component={AppPage}>
+          <IndexRoute title="Home" component={HomePage}></IndexRoute>
 
-          <Route path="/accounts" title="Accounts" component={AccountsPage}></Route>
-          <Route path="/accounts/create" title="Create Account" component={AccountCreatePage}></Route>
-          <Route path="/accounts/:accountId" title="Account View" component={AccountViewPage}></Route>
+          <Route path="accounts" title="Accounts" component={AccountsPage}></Route>
+          <Route path="accounts/create" title="Create Account" component={AccountCreatePage}></Route>
+          <Route path="accounts/:accountId" title="Account View" component={AccountViewPage}></Route>
 
-          <Route path="/campaigns" title="Campaigns" component={CampaignsPage}></Route>
-          <Route path="/campaigns/:campaignId" title="Campaign View" component={CampaignViewPage}></Route>
-          <Route path="/campaigns/:campaignId/bulkupload" title="Campaign Bulk Upload" component={CampaignBulkUploadPage}></Route>
+          <Route path="campaigns" title="Campaigns" component={CampaignsPage}></Route>
+          <Route path="campaigns/:campaignId" title="Campaign View" component={CampaignViewPage}></Route>
+          <Route path="campaigns/:campaignId/bulkupload" title="Campaign Bulk Upload" component={CampaignBulkUploadPage}></Route>
 
-          <Route path="/adgroups" title="Ad Groups" component={AdGroupsPage}></Route>
-          <Route path="/adgroups/:adGroupId" title="Ad Group View" component={AdGroupViewPage}></Route>
+          <Route path="adgroups" title="Ad Groups" component={AdGroupsPage}></Route>
+          <Route path="adgroups/:adGroupId" title="Ad Group View" component={AdGroupViewPage}></Route>
 
-          <Route path="/tiles" title="Tiles" component={TilesPage}></Route>
-          <Route path="/tiles/:tileId" title="Tile View" component={TileViewPage}></Route>
+          <Route path="tiles" title="Tiles" component={TilesPage}></Route>
+          <Route path="tiles/:tileId" title="Tile View" component={TileViewPage}></Route>
 
-          <Route path="/approvals" title="Approval Queue" component={ApprovalsPage}></Route>
+          <Route path="approvals" title="Approval Queue" component={ApprovalsPage}></Route>
 
-          <Route path="/error404" title="Page Not Found" component={Error404Page}></Route>
+          <Route path="error404" title="Page Not Found" component={Error404Page}></Route>
+          <Route path="*" component={Error404Page}/>
         </Route>
       </Router>
     );
