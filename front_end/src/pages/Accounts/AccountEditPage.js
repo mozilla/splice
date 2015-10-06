83c55a71df5eef8dc7c668a763ac54acc349b669
Tawin Kiethanom <tawin.k@gmail.com>
2015-10-06 10:11:28 -0700

    Working Account Form Validation. Moved control to AccountForm.

--- front_end/src/pages/Accounts/AccountEditPage.js
+++ front_end/src/pages/Accounts/AccountEditPage.js
@@ -29,39 +29,12 @@ export default class AccountEditPage extends Component {
         <div className="panel panel-default">
           <div className="panel-body">
             { (this.props.Account.details.id !== undefined)
-              ? <AccountForm isSaving={this.props.Account.isSaving} handleFormSubmit={(id) => this.handleFormSubmit(id)} data={this.props.Account.details} editMode={true} dispatch={this.props.dispatch}/>
+              ? <AccountForm isSaving={this.props.Account.isSaving} data={this.props.Account.details} editMode={true} dispatch={this.props.dispatch} history={this.props.history}/>
               : null
             }
           </div>
         </div>
       </div>
-    );
-  }
-
-  handleFormSubmit(id){
-    const { dispatch } = this.props;
-    const props = this.props;
-    const context = this;
-
-    const data = JSON.stringify($(id).serializeJSON());
-
-    dispatch(updateAccount(this.props.Account.details.id, data))
-      .then(function(response){
-        if(response.result === undefined){
-          if(_.isString(response.message)){
-            dispatch(displayMessage('error', 'Error: ' + response.message) );
-          }
-          else{
-            dispatch(displayMessage('error', 'Error: Validation Errors') );
-          }
-          dispatch(shownMessage());
-        }
-        else{
-          dispatch(fetchAccounts());
-          dispatch(displayMessage('success', 'Account Updated Successfully') );
-          dispatch(shownMessage());
-        }
-      }
     );
   }
 
