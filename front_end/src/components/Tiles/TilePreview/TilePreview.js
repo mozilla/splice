d3f542f2940cb62de9d2d4e2a3310f20a0416041
Tawin Kiethanom <tawin.k@gmail.com>
2015-10-19 14:32:27 -0700

    Added ability to approve and disapprove tiles.

--- front_end/src/components/Tiles/TilePreview/TilePreview.js
+++ front_end/src/components/Tiles/TilePreview/TilePreview.js
@@ -8,14 +8,26 @@ export default class TilePreview extends Component {
     let output = (<div/>);
     if(this.props.Tile.details.image_uri){
       output = (
-        <div className="tile-preview-container">
-          <div className="tile-preview tile-default">
-            <div className="tile-image" style={{backgroundColor: this.props.Tile.details.bg_color, backgroundImage: 'url(' + this.props.Tile.details.image_uri + ')' }}></div>
-            <div className="tile-title" style={{backgroundColor: this.props.Tile.details.title_bg_color}}>{this.props.Tile.details.title}</div>
+        <div>
+          <div className="tiles-container">
+            <div className="tile-preview-container tile-default" >
+              <div className="tile-preview">
+                <div className="tile-image" style={{backgroundColor: this.props.Tile.details.bg_color, backgroundImage: 'url(' + this.props.Tile.details.image_uri + ')' }}></div>
+                <div className="tile-title" style={{backgroundColor: this.props.Tile.details.title_bg_color}}>{this.props.Tile.details.title}</div>
+              </div>
+              <p className="tile-description">Tile Preview</p>
+            </div>
+            <div className="tile-preview-container tile-hover">
+              <div className="tile-preview">
+                <div className="tile-image" style={{backgroundColor: this.props.Tile.details.bg_color, backgroundImage: 'url(' + this.props.Tile.details.enhanced_image_uri + ')' }}></div>
+                <div className="tile-title" style={{backgroundColor: this.props.Tile.details.title_bg_color}}>{this.props.Tile.details.title}</div>
+              </div>
+              <p>Rollover Preview</p>
+            </div>
           </div>
-          <div className="tile-preview tile-hover">
-            <div className="tile-image" style={{backgroundColor: this.props.Tile.details.bg_color, backgroundImage: 'url(' + this.props.Tile.details.enhanced_image_uri + ')' }}></div>
-            <div className="tile-title" style={{backgroundColor: this.props.Tile.details.title_bg_color}}>{this.props.Tile.details.title}</div>
+          <div className="tile-status-buttons">
+            <input type="submit" className="btn disapprove" value="Disapprove" onClick={() => this.props.handleDisapprove()}/>
+            <input type="submit" className="btn approve" value="Approve" onClick={() => this.props.handleApprove()}/>
           </div>
         </div>
       );
@@ -25,5 +37,7 @@ export default class TilePreview extends Component {
 }
 
 TilePreview.propTypes = {
-  Tile: PropTypes.object.isRequired
+  Tile: PropTypes.object.isRequired,
+  handleApprove: PropTypes.func.isRequired,
+  handleDisapprove: PropTypes.func.isRequired
 };
