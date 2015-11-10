f71113d1daa8779a76a27627699c68e7ef803e38
Ricky Rosario <rickyrosario@gmail.com>
2015-11-10 09:18:38 -0500

    Add distribution previewer UI.

---
+++ ui/src/components/Picker.js
@@ -0,0 +1,30 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Picker extends Component {
+  render() {
+    const { title, value, onChange, options } = this.props;
+
+    return (
+      <span className="picker">
+        <label>{title + ':'}</label>
+        <select onChange={e => onChange(e.target.value)}
+                value={value}>
+          {options.map(option =>
+            <option value={option} key={option}>
+              {option}
+            </option>)
+          }
+        </select>
+      </span>
+    );
+  }
+}
+
+Picker.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.string.isRequired
+  ).isRequired,
+  title: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+};
---
+++ ui/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import { WebtilesPreviewer } from './WebtilesPreviewer.js';
+
+const rootReducer = combineReducers({
+  WebtilesPreviewer
+});
+
+export default rootReducer;
