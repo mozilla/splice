790f7770361acdac1211a8d07689f69554ff4ce5
Ricky Rosario <rickyrosario@gmail.com>
2015-11-17 16:49:27 -0500

    Update distribution app dependencies.

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,15 +1,16 @@
 import 'babel-core/polyfill';
 
 import React from 'react';
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux';
 import Routes from './containers/Routes';
 import configureStore from './store/configureStore';
 
 const store = configureStore({});
 
-React.render(
+ReactDOM.render(
   <Provider store={store}>
-    {() => <Routes />}
+    <Routes />
   </Provider>,
   document.getElementById('root')
 );
