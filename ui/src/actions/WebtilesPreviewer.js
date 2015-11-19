44faa09816eb79fe232e9928c96e4b02af165828
Ricky Rosario <rickyrosario@gmail.com>
2015-11-19 11:35:28 -0500

    Implement distribution viewer by date using API.

--- ui/src/actions/WebtilesPreviewer.js
+++ ui/src/actions/WebtilesPreviewer.js
@@ -1,12 +1,19 @@
 import fetch from 'isomorphic-fetch';
 
-export const REQUEST_LOCALES = 'REQUEST_LOCALES';
-export const RECEIVE_LOCALES = 'RECEIVE_LOCALES';
-export const REQUEST_TILES = 'REQUEST_TILES';
-export const RECEIVE_TILES = 'RECEIVE_TILES';
+export const LOAD_DISTRIBUTION_START = 'LOAD_DISTRIBUTION_START';
+export const LOAD_DISTRIBUTION_SUCCESS = 'LOAD_DISTRIBUTION_SUCCESS';
+export const LOAD_DISTRIBUTION_ERROR = 'LOAD_DISTRIBUTION_ERROR';
+export const SELECT_DATE = 'SELECT_DATE';
 export const SELECT_CHANNEL = 'SELECT_CHANNEL';
 export const SELECT_LOCALE = 'SELECT_LOCALE';
 export const SELECT_TYPE = 'SELECT_TYPE';
+
+export function selectDate(moment) {
+  return {
+    type: SELECT_DATE,
+    moment
+  };
+}
 
 export function selectChannel(channel) {
   return {
@@ -29,97 +36,69 @@ export function selectType(tileType) {
   };
 }
 
-function requestLocales(channel) {
-  return {
-    type: REQUEST_LOCALES,
-    channel
-  };
-}
-
-function requestTiles(channel, locale) {
-  return {
-    type: REQUEST_TILES,
-    channel,
-    locale
-  };
-}
-
-function receiveLocales(channel, json) {
-  // Massage the json results into what we want.
-  var locales = {};
-  Object.keys(json).map(function(value, index) {
-    locales[value] = {
-      tileIndexUrl: json[value].ag,
-      isFetching: false
-    };
-  });
-  return {
-    type: RECEIVE_LOCALES,
-    channel: channel,
-    locales: locales,
-    receivedAt: Date.now()
-  };
-}
-
-function receiveTiles(channel, locale, json) {
-  return {
-    type: RECEIVE_TILES,
-    channel: channel,
-    locale: locale,
-    directoryTiles: json.directory,
-    suggestedTiles: json.suggested,
-    receivedAt: Date.now()
-  };
-}
-
-function fetchLocales(state, channel) {
-  return dispatch => {
-    dispatch(requestLocales(channel));
-    return fetch(state.channels[channel].localeIndexUrl)
-      .then(response => response.json())
-      .then(json => dispatch(receiveLocales(channel, json)));
-  };
-}
-
-function fetchTiles(state, channel, locale) {
-  return dispatch => {
-    dispatch(requestTiles(channel, locale));
-    return fetch(state.channels[channel].locales[locale].tileIndexUrl)
-      .then(response => response.json())
-      .then(json => dispatch(receiveTiles(channel, locale, json)));
-  };
-}
-
-function shouldFetchLocales(state, channel) {
-  const channelObj = state.channels[channel];
-  if (!channelObj.locales && !channelObj.isFetching) {
-    return true;
-  }
-  return false;
-}
-
-function shouldFetchTiles(state, channel, locale) {
-  const localeObj = state.channels[channel].locales[locale];
-  if (localeObj && !localeObj.directoryTiles && !localeObj.isFetching) {
-    return true;
-  }
-  return false;
-}
-
-export function fetchLocalesIfNeeded(channel) {
-  return (dispatch, getState) => {
-    if (shouldFetchLocales(getState().WebtilesPreviewer, channel)) {
-      return dispatch(fetchLocales(getState().WebtilesPreviewer, channel));
-    } else {
-      return dispatch(selectLocale(Object.keys(getState().WebtilesPreviewer.channels[channel].locales)[0]));
+function checkStatus(response){
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    if (response.status === 404) {
+      throw new Error('No distribution found for this date.');
     }
-  };
+    throw new Error('HTTP ' + response.status + ' - ' + response.statusText);
+  }
 }
 
-export function fetchTilesIfNeeded(channel, locale) {
+export function fetchDistribution(moment) {
   return (dispatch, getState) => {
-    if (shouldFetchTiles(getState().WebtilesPreviewer, channel, locale)) {
-      return dispatch(fetchTiles(getState().WebtilesPreviewer, channel, locale));
-    }
+    dispatch((() => ({type: LOAD_DISTRIBUTION_START}))());
+
+    let url = getState().WebtilesPreviewer.distribution.fetchUrl;
+    url += '?date=' + moment.format('YYYY-MM-D')
+    return fetch(url)
+      .then(checkStatus)
+      .then(response => response.json())
+      .then(json => {
+        // Process the results
+        let channels = {};
+        Object.keys(json.results).forEach(function(channel) {
+          channels[channel] = {};
+
+          // Process the S3 artifacts into something more usable
+          let index;
+          let otherArtifacts = {};
+          json.results[channel].forEach(function(artifact) {
+            if (artifact.key.includes('index')) {
+              // The index file key will be something like: desktop_tile_index.v3.json
+              index = JSON.parse(artifact.data);
+            } else {
+              otherArtifacts[artifact.key] = JSON.parse(artifact.data);
+            }
+          });
+
+          // Process all the locales in the index
+          Object.keys(index).forEach(function(locale) {
+            if (locale === '__ver__') return;  // skip that garbage
+
+            // Get the artifact key
+            // TODO: Handle multiple distributions if passed
+            let agUrl = Array.isArray(index[locale].ag) ? index[locale].ag[0] : index[locale].ag;
+            let key = agUrl.split('/').slice(3).join('/');
+
+            // Get and set the tiles
+            channels[channel][locale] = otherArtifacts[key];
+          });
+        });
+
+        // And that's all folks. Let the World know.
+        dispatch((() => ({
+          type: LOAD_DISTRIBUTION_SUCCESS,
+          results: channels
+        }))());
+      })
+      .catch(error => {
+        dispatch((() => ({
+          type: LOAD_DISTRIBUTION_ERROR,
+          message: error.toString()
+        }))());
+      });
   };
 }
