9a29058a3e831c27864e8d656126bf7ca66bb0f5
Ricky Rosario <rickyrosario@gmail.com>
2016-01-25 12:06:25 -0500

    Cut version 2.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="splice",
-    version="2.0.3",
+    version="2.0.4",
     description="Link authoring tool for " +
                 "Firefox Directory and Enhanced Tiles",
     author="Mozilla",
