04b334c7610a7494354a704116c59bc42a593de4
Nan Jiang <njiang028@gmail.com>
2016-03-14 15:40:53 -0400

    Add various tweaks on schema for activity_stream
    Close #328
    
    Squashed commit of the following:
    
    commit 90d09491217be25895eea641ea57faf87f5f67da
    Author: Nan Jiang <njiang028@gmail.com>
    Date:   Mon Mar 14 15:00:24 2016 -0400
    
        Add load_latency to activity_stream
    
    commit 3b5cb2eb12b730f335adb811aae727b5a6d4d3b7
    Author: Nan Jiang <njiang028@gmail.com>
    Date:   Thu Mar 10 16:02:11 2016 -0500
    
        Alter activity_stream_stats_daily table

--- ddl/migrations/redshift/1457402851.sql
+++ ddl/migrations/redshift/1457402851.sql
@@ -1,14 +1,17 @@
 BEGIN;
 
+DROP TABLE IF EXISTS activity_stream_stats_daily;
+
 CREATE TABLE activity_stream_stats_daily (
     client_id VARCHAR(64) NOT NULL, 
-    tab_id INTEGER NOT NULL, 
+    tab_id VARCHAR(64) NOT NULL,
     addon_version VARCHAR(16) NOT NULL, 
     load_reason VARCHAR(64) NOT NULL, 
     source VARCHAR(64) NOT NULL, 
     unload_reason VARCHAR(64) NOT NULL, 
     max_scroll_depth INTEGER NOT NULL, 
-    click_position VARCHAR(16) NOT NULL, 
+    load_latency INTEGER NOT NULL,
+    click_position INTEGER NOT NULL,
     total_bookmarks INTEGER NOT NULL, 
     total_history_size INTEGER NOT NULL, 
     session_duration INTEGER NOT NULL, 
