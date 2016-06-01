ef17f1300e3d96a73c3481afe1ac00c73b9f07ad
Nan Jiang <njiang028@gmail.com>
2016-06-01 10:42:46 -0400

    Fix #350 - add table for Activity Stream performance
    Close #351
    
    Squashed commit of the following:
    
    commit fd605a8423083414bd5c9795724dcb4867085556
    Author: Nan Jiang <njiang028@gmail.com>
    Date:   Wed Jun 1 09:48:21 2016 -0400
    
        Add table for activity_stream_performance

--- ddl/migrations/redshift/1464292273.sql
+++ ddl/migrations/redshift/1464292273.sql
@@ -1,6 +1,32 @@
 BEGIN;
 
+ALTER TABLE activity_stream_events_daily ADD COLUMN session_id VARCHAR(64);
+
 ALTER TABLE activity_stream_events_daily ADD COLUMN experiment_id VARCHAR(64);
+
+ALTER TABLE activity_stream_stats_daily ADD COLUMN session_id VARCHAR(64);
+
 ALTER TABLE activity_stream_stats_daily ADD COLUMN experiment_id VARCHAR(64);
+
+
+CREATE TABLE activity_stream_performance_daily (
+    client_id VARCHAR(64) NOT NULL, 
+    tab_id VARCHAR(64) NOT NULL, 
+    addon_version VARCHAR(64) NOT NULL, 
+    source VARCHAR(64) NOT NULL, 
+    session_id VARCHAR(64), 
+    experiment_id VARCHAR(64), 
+    event VARCHAR(64) NOT NULL, 
+    event_id VARCHAR(64) NOT NULL, 
+    value INTEGER NOT NULL, 
+    receive_at TIMESTAMP WITHOUT TIME ZONE NOT NULL, 
+    date DATE NOT NULL, 
+    locale VARCHAR(14) NOT NULL, 
+    country_code VARCHAR(5) NOT NULL, 
+    os VARCHAR(64) NOT NULL, 
+    browser VARCHAR(64) NOT NULL, 
+    version VARCHAR(64) NOT NULL, 
+    device VARCHAR(64) NOT NULL
+);
 
 COMMIT;
