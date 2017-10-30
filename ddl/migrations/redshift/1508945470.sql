7d44edffc19be1b570a08df3065c05bb23ba686b
Nan Jiang <njiang028@gmail.com>
2017-10-30 14:35:35 -0400

    Add no_image to AS session

--- ddl/migrations/redshift/1508945470.sql
+++ ddl/migrations/redshift/1508945470.sql
@@ -16,6 +16,8 @@ ALTER TABLE assa_sessions_daily ADD COLUMN screenshot_with_icon INTEGER;
 
 ALTER TABLE assa_sessions_daily ADD COLUMN tippytop INTEGER;
 
+ALTER TABLE assa_sessions_daily ADD COLUMN no_image INTEGER;
+
 ALTER TABLE assa_impression_stats_daily ADD COLUMN hour INTEGER;
 
 ALTER TABLE assa_impression_stats_daily ADD COLUMN minute INTEGER;
