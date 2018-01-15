f3b00eb474d9a1c572c2c002f0c2d6f7d1abe336
Nan Jiang <njiang028@gmail.com>
2018-01-15 10:47:59 -0500

    Add topsites_pinned, profile_creation_date, and client_region to AS

---
+++ ddl/migrations/redshift/1516031096.sql
@@ -0,0 +1,11 @@
+BEGIN;
+
+ALTER TABLE assa_impression_stats_daily ADD COLUMN client_region VARCHAR(5);
+
+ALTER TABLE assa_sessions_daily ADD COLUMN client_region VARCHAR(5);
+
+ALTER TABLE assa_sessions_daily ADD COLUMN profile_creation_date INTEGER;
+
+ALTER TABLE assa_sessions_daily ADD COLUMN topsites_pinned INTEGER;
+
+COMMIT;
