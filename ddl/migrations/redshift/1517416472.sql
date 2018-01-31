3b952cf959ea01df3f5e7bb38c8e1f426608f44a
Nan Jiang <njiang028@gmail.com>
2018-01-31 11:37:13 -0500

    Drop unused tables for activity stream and test pilot

---
+++ ddl/migrations/redshift/1517416472.sql
@@ -0,0 +1,25 @@
+BEGIN;
+
+DROP TABLE activity_stream_impression_daily;
+
+DROP TABLE ss_session;
+
+DROP TABLE ss_performance;
+
+DROP TABLE activity_stream_stats_daily;
+
+DROP TABLE ping_centre_test_pilot;
+
+DROP TABLE activity_stream_masga;
+
+DROP TABLE ss_masga;
+
+DROP TABLE ss_impression;
+
+DROP TABLE ss_event;
+
+DROP TABLE activity_stream_events_daily;
+
+DROP TABLE activity_stream_performance_daily;
+
+COMMIT;
